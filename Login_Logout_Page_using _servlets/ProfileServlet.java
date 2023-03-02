626da2324a430a63ae23b14521d522d4eae2067e
Teja Vardhan <104910147+Tej0901@users.noreply.github.com>
2023-03-02 12:03:11 +0530

    Add files via upload

---
+++ Login_Logout_Page_using _servlets/ProfileServlet.java
@@ -0,0 +1,37 @@
+package com.login.teja;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProfileServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("Link.html").include(request, response);
+		
+		Cookie cookies[] = request.getCookies();
+		if(cookies!=null)
+		{
+			String name=cookies[0].getValue();
+			if(!name.equals("") || name!=null)
+			{
+				out.println("Profile Page....");
+				out.println("<br>Welcome "+name);
+			}
+		}
+		else
+		{
+			out.println("Please login First to Access!!!!!");
+			request.getRequestDispatcher("login.html").include(request, response);
+		}
+		out.close();
+	}
+}
